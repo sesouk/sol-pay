c760c53c1b8386256bd5b849f0eaf00b54b08538
Kevin Tran <sesouk@gmail.com>
2022-04-24 17:36:08 -0600

    get coupons

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -2,3 +2,4 @@ import { PublicKey } from "@solana/web3.js";
 
 export const shopAddress = new PublicKey('EQVsZofCQaH79ajTJDXSbyAopWUWmmWddnBAzZzCp23v')
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('AbNvXPKQ1ixcqqce2zVpTJiDhq5ceTAbuTBxbJ7ZLieS')
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useWallet } from '@solana/wallet-adapter-react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
+import CouponBook from '../components/CouponBook'
 
 export default function HomePage() {
   const { publicKey } = useWallet()
@@ -13,6 +14,8 @@ export default function HomePage() {
       <div className='basis-1/4'>
         <WalletMultiButton className='!bg-gray-900 hover:scale-105' />
       </div>
+
+      {publicKey && <CouponBook />}
 
       <Products submitTarget='/checkout' enabled={publicKey !== null} />
     </div>
